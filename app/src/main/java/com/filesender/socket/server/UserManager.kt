45397a7d282593c96eb961b8ad58ed787e4c5ad1
Федотов Яков Олегович <ya.fedotov@digipeople.ru>
2023-04-24 16:31:13 +0300

    123

--- app/src/main/java/com/filesender/socket/server/UserManager.kt
+++ app/src/main/java/com/filesender/socket/server/UserManager.kt
@@ -132,7 +132,6 @@ class UserManager(
             CommandType.FILE -> {
                 command = gson.fromJson(message, File::class.java)
                 socketState = SocketServerState.File(command.toModel)
-                isFile = true
                 mutex.withLock {
                     //receiveFile(socketState.file.data, socketState.file.size)
                 }
@@ -147,66 +146,6 @@ class UserManager(
         return socketState
     }
 
-    var isFile = false
-
-    private fun receiveFile(fileName: String, size: Int) {
-        val file = java.io.File(Environment.getExternalStorageDirectory().absolutePath + "/" +fileName)
-        val startTime = System.currentTimeMillis()
-        Log.e("TAG", fileName)
-        Log.e("TAG", size.toString())
-
-        var `in`: InputStream? = null
-        var out: OutputStream? = null
-
-
-        try {
-            `in` = socket!!.getInputStream()
-        } catch (ex: IOException) {
-            Log.e("TAG", "Error " + ex.message ?: "")
-        }
-        Log.e("TAG", fileName)
-
-        try {
-            out = FileOutputStream(file)
-        } catch (ex: FileNotFoundException) {
-            Log.e("TAG", "Error " + ex.message ?: "")
-        }
-        Log.e("TAG", fileName)
-
-        val bytes = ByteArray(file.length().toInt())
-
-        Log.e("TAG", "bytes " + bytes[0] ?: "")
-
-        var count: Int
-        while (`in`!!.read(bytes).also { count = it } > 0) {
-            out!!.write(bytes, 0, count)
-        }
-
-        out!!.close()
-        `in`!!.close()
-        Log.e("TAG", "time: "+(TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis() - startTime)).toString())
-
-    }
-
-    private fun receiveFile2(fileName: String, size: Int) {
-
-        val startTime = System.currentTimeMillis()
-
-        Log.e("TAG", fileName)
-        val mybytearray = ByteArray(size)
-        Log.e("TAG", size.toString())
-        val `is`: InputStream = socket!!.getInputStream()
-        val fos = FileOutputStream(Environment.getExternalStorageDirectory().absolutePath + "/" +fileName)
-        val bos = BufferedOutputStream(fos)
-        val bytesRead = `is`.read(mybytearray, 0, mybytearray.size)
-        //Log.e("TAG", bytesRead.toString())
-        bos.write(mybytearray, 0, bytesRead)
-        bos.close()
-        socket!!.close()
-        isFile = false
-
-        Log.e("TAG", "time: "+(TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis() - startTime)).toString())
-    }
 
 
     private fun processReceivedWithoutUi(command: BaseCommand) {
@@ -217,86 +156,6 @@ class UserManager(
             }
 
             else -> return
-        }
-    }
-
-    private fun receiveFile2(fileName: String) {
-
-        try {
-            val dataInputStream = DataInputStream(socket!!.getInputStream())
-
-            Log.e("TAG", Environment.getExternalStorageDirectory().path)
-
-            var bytes = -0
-            val fileOutputStream =
-                FileOutputStream(Environment.getExternalStorageDirectory().absolutePath + "/" + fileName)
-            Log.e("TAG", "cool1")
-
-            var size: Long = dataInputStream.readLong() // read file size
-            val buffer = ByteArray(4 * 1024)
-            Log.e("TAG", "cool2")
-
-            while (size > 0
-                && dataInputStream.read(
-                    buffer, 0, Math.min(buffer.size.toLong(), size).toInt()
-                ).also { bytes = it }
-                != -1
-            ) {
-                // Here we write the file using write method
-                fileOutputStream.write(buffer, 0, bytes)
-                size -= bytes.toLong() // read upto file size
-            }
-            Log.e("TAG", "cool3")
-
-            // Here we received file
-            println("File is Received")
-            fileOutputStream.flush()
-            fileOutputStream.close()
-        } catch (e: java.lang.Exception) {
-            e.message
-            Log.e("TAG", "err " +e.message)
-        }
-    }
-
-    fun getInternalStorageDirectoryPath(context: Context): String? {
-        val storageDirectoryPath: String?
-        storageDirectoryPath =
-            Environment.getExternalStorageDirectory().absolutePath
-        return storageDirectoryPath
-    }
-
-    private fun fileReceived(name: String) {
-
-
-        if (socket == null) {
-            return
-        }
-
-        var current = 0
-        var fos: FileOutputStream? = null
-        var bos: BufferedOutputStream? = null
-        try {
-
-            // receive file
-            val mybytearray = ByteArray(64 * 1024)
-            val inputStream: InputStream = socket!!.getInputStream()
-            fos = FileOutputStream(Environment.getExternalStorageDirectory().absolutePath + "/" +name)
-            bos = BufferedOutputStream(fos)
-            var bytesRead = inputStream.read(mybytearray, 0, mybytearray.size)
-            current = bytesRead
-            do {
-                bytesRead = inputStream.read(mybytearray, current, mybytearray.size - current)
-                if (bytesRead >= 0) current += bytesRead
-            } while (bytesRead > -1)
-            bos.write(mybytearray, 0, current)
-            bos.flush()
-
-        } catch (e: Exception) {
-            sendMessage(e.message ?: "yt")
-            Log.e("TAG", "err " +e.message)
-        } finally {
-            fos?.close()
-            bos?.close()
         }
     }
 
